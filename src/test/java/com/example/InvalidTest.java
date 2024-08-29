fbee4f48c76540bb94e666319a35bb653ceef5fb
Manish Dait <daitmanish88@gmail.com>
2024-08-29 21:16:53 +0530

    added invalid test case fo jest

---
+++ src/test/java/com/example/InvalidTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class InvalidTest {
+  @Test
+  public void invalidTest() {
+    assertEquals(1, 2);
+  }
+}
